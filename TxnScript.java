2df4cd29439ca2fd1fb2122ecac89eaafdff466f
BAUCHER Frederic <frederic.baucher@cegedim.com>
2021-05-14 11:18:20 +0200

    init sur la base de EXB1610 local

---
+++ TxnScript.java
@@ -0,0 +1,350 @@
+// squelette du TxnScript de EXB1618
+
+import java.util.Date;
+
+
+// types retournés par les opérations JDBC
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.CallableStatement ; 
+import java.sql.Connection ; 
+import java.sql.DriverManager ; 
+import java.sql.Statement ; 
+import java.sql.ResultSet ; 
+import java.sql.PreparedStatement ;
+import java.sql.CallableStatement ;
+import java.sql.SQLException ;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class TxnScript
+{
+
+	// Variable générique
+	private static String jdbcUrl = "" ;
+	private static String jdbcMachine = "" ;
+	private static String jdbcDatabase = "" ;
+	private static String jdbcUser = "" ;
+	private static String jdbcPass = "" ;
+	
+	// exemple MYSQL LOCAL
+	private static String jdbcMysqlMachine = "localhost" ;
+	private static String jdbcMysqlUser = "root" ;
+	private static String jdbcMysqlPass = "" ; // In MySQL, by default, the username is root and there’s no password.
+	private static String jdbcMysqlIntricacies = "zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC" ;
+
+	private static Connection theCnx = null ;
+
+	// le constructeur de TxnScript établit la connexion
+    private TxnScript ()
+	{
+		boolean dbAbsent = true ;
+
+		// si MySQL
+		jdbcUrl = "" ;
+		jdbcMachine = jdbcMysqlMachine  ;
+		jdbcDatabase = "ANNUAIRE" ;
+		jdbcUser = jdbcMysqlUser ;
+		jdbcPass = jdbcMysqlPass ;
+
+		// Création de la base de données, si elle n'existe password
+		// Une connection dédiée est utilisée
+		try
+		{		
+			jdbcUrl = "jdbc:mysql://" + jdbcUser + ":" + jdbcPass + "@" + jdbcMachine + ":3306";
+			theCnx = DriverManager.getConnection( jdbcUrl ) ;
+			Statement theStmt = theCnx.createStatement();
+		
+			// vérifier si la base existe déjà
+			ResultSet resultSet = theCnx.getMetaData().getCatalogs();
+
+			//iterate each catalog in the ResultSet
+			while (resultSet.next())
+			{
+				// Get the database name, which is at position 1
+				String databaseName = resultSet.getString(1).toUpperCase() ;
+				if ( databaseName.equals ( jdbcDatabase ) )
+				{
+					dbAbsent = false ;
+				}
+			}
+			resultSet.close();
+		
+			if ( dbAbsent == true )
+			{
+				String sqlCreateDb = "CREATE DATABASE " + jdbcDatabase ;
+				theStmt.execute( sqlCreateDb );
+			}
+			// theCnx.commit(); // now the database physically exists. If auto-commit activated => fails 
+			theCnx.close();
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		// Ouverture d'une connection sur la base, pour les futurs requêtes appelées via les appels aux fonctions de tnxscript.
+		try
+		{		
+			jdbcUrl = "jdbc:mysql://" + jdbcUser + ":" + jdbcPass + "@" + jdbcMachine + ":3306" + "/" + jdbcDatabase ;
+			theCnx = DriverManager.getConnection( jdbcUrl ) ;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+    }
+
+    public static TxnScript getTxnScript() {
+        return new TxnScript();
+    }
+	
+
+    // cette fonction doit initialiser la structure (DDL) et les éventuels enregistrements au démarrage
+    public static boolean initDb()	
+    {
+		// TO DO
+	    boolean the_result = true ;
+		String the_sql = "" ;
+
+		try
+		{
+			PreparedStatement pstmt = theCnx.prepareStatement(the_sql) ;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+
+	    return the_result ;
+    }    
+
+
+
+
+    public static boolean populateDb()	
+    {
+		// TO DO
+	    boolean the_result = true ;
+		String the_sql = "" ;
+		
+		try
+		{
+			PreparedStatement pstmt = theCnx.prepareStatement(the_sql) ;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	    return the_result ;
+    }    
+
+
+
+
+
+	
+    // cette fonction doit retirer toute la structure de la base
+    public static  boolean eraseDb()	
+    {
+		// TO DO
+	    boolean the_result = true ;
+		String the_sql = "" ;
+		
+		try
+		{
+			PreparedStatement pstmt = theCnx.prepareStatement(the_sql) ;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	    return the_result ;
+	}
+	
+    // cette fonction doit vérifier que la connection avec le SGBD est opérationnelle et vérifier que la base est correcte
+	// (présence des tables, par exemple)
+    public static boolean check()
+    {
+		// TO DO
+	    boolean the_result = true ;
+		String the_sql = "" ;
+		
+		try
+		{
+			PreparedStatement pstmt = theCnx.prepareStatement(the_sql) ;
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	    return the_result ;
+    }
+
+    /*************** ITERATEUR AVEC LA TABLE VILLES ***************/
+
+	public class VillesIterator
+	{
+		PreparedStatement monTraitement ;
+		ResultSet maListe ;
+		
+		private VillesIterator ( String the_sql )
+		{
+			try
+			{
+				if ( the_sql != "" )
+				{
+					monTraitement = theCnx.prepareStatement(the_sql) ;
+					maListe = monTraitement.executeQuery();	
+				}
+				else
+				{
+					monTraitement = null ;
+					maListe = null ;	
+				}
+			}
+			catch (SQLException e)
+			{
+				System.out.println(e.getMessage());
+			}			
+		}
+		
+		boolean hasNext()
+		{
+			boolean the_result = false ;
+			try
+			{
+				if ( maListe != null )
+				{
+					the_result = maListe.next();
+				}
+				else
+				{
+					the_result = false ;
+				}
+			}
+			catch (SQLException e)
+			{
+				System.out.println(e.getMessage());
+			}			
+			
+
+			return the_result ;
+		}
+
+		Integer nextIdVille()
+		{
+			Integer the_result = 0 ;
+
+			try
+			{
+				the_result = maListe.getInt( "id" ) ;
+			}
+			catch (SQLException e)
+			{
+				System.out.println(e.getMessage());
+			}			
+			return the_result ;
+		}
+		
+		
+		String nextNomVille()
+		{
+			String the_result ="" ;
+			try
+			{
+				the_result = maListe.getString( "nom" ) ;
+			}
+			catch (SQLException e)
+			{
+				System.out.println(e.getMessage());
+			}			
+			return the_result ;
+		}
+
+		Integer nextCodePostalVille()
+		{
+			Integer the_result = 0 ;
+			try
+			{
+				the_result = maListe.getInt( "code_postal" ) ;
+			}
+			catch (SQLException e)
+			{
+				System.out.println(e.getMessage());
+			}			
+			return the_result ;
+		}
+	}
+	
+	
+    // insére un enregistrement dans la table Ville
+	// renvoie l'id
+    public static Integer insertVille (String nom, Integer codePostal)
+    {
+		// TO DO		
+	    Integer the_result = 0 ;
+	    return the_result ;
+    }
+
+	
+    // recherche tous les enregistrements de la table Ville dont le code postal correspondant
+    public VillesIterator getIterator4searchByCodePostal(Integer searchedCodePostal)
+    {
+		// TO DO		
+		String the_sql = "" ;
+		VillesIterator the_it = null ;
+		
+		return the_it ;
+    }
+
+
+	// retirer (effacer) un enregistrement de la table, en fonction de l'id
+	// renvoyer false si aucun enregistrement n'est trouvé avec cet id
+    public boolean deleteVille(Integer id)
+    {
+		boolean the_result = false ;
+		String the_sql = "" ;
+		
+		// TO DO
+
+		return the_result ;
+    }
+
+
+    public VillesIterator getIterator4listerVilles()
+    {
+		// TO DO
+		String the_sql = "" ;
+		VillesIterator the_it = new VillesIterator(the_sql) ;
+		return the_it ;
+    }
+
+
+    // met à jour un enregistrement existant dans la table Ville : renvoie une chaine avec les valeurs intégrées dans la table	
+    public static String updateVille (Integer id, String nom, Integer codePostal)
+    {
+        String the_result = "" ;
+		// TO DO
+		return the_result ;
+    }
+	
+
+    // clot la connection au SGBD et renvoie true si la fermeture s'est bien déroulée, false sinon
+    private boolean close()
+    {
+		boolean the_result = true ;
+		return the_result ;
+    }
+}
---
+++ simplissimeCmdline.java
@@ -0,0 +1,142 @@
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.ParseException;
+
+import java.lang.reflect.Array;
+
+public class simplissimeCmdline {
+
+	private static TxnScript txn = null ;
+	private static String saut_de_ligne = "\n" ;
+
+    public static void main(String[] args)
+	{
+		
+        try {
+            Options opt = new Options();
+
+            opt.addOption("h", false, "Aide de cette application en mode ligne de commande");
+            opt.addOption("c", false, "[C]rud : ajouter une ville avec le nom et le code postal (nom, code postal)");
+            opt.addOption("r", false, "c[R]ud : retrouver une ville en fonction d'un parametre (id, nom, code postal)");
+            opt.addOption("u", false, "cr[U]d : mettre a jour une ville avec l'id, le nom et le code postal > simplissimeCmdline -u -id 21 -nom MARRAKECH -cp 40160");
+            opt.addOption("d", false, "cru[D] : effacer une ville en fonction du parametre fourni (id)");
+            opt.addOption("l", false, "lister tous les enregistrements de la table villes");
+            opt.addOption("v", false, "vérifier les acces a la base (pilote, ...)");
+            opt.addOption("z", false, "ecraser la base");
+            opt.addOption("i", false, "initialiser la base");
+            opt.addOption("p", false, "peupler la base");
+            opt.addOption("nom", true, "nom de la ville" );
+            opt.addOption("cp", true, "code postal de la ville" );
+            opt.addOption("id", true, "identifiant de la ville" );
+			
+            DefaultParser parser = new DefaultParser();
+            CommandLine cl = parser.parse(opt, args);
+
+			// afficher une aide qui documente chaque option
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -h
+            if ( cl.hasOption('h') ) {
+                HelpFormatter f = new HelpFormatter();
+                f.printHelp("Aide de simplissime", opt);
+            }
+		
+	    // Initialisation des bases
+
+			String display = "" ;
+			txn = TxnScript.getTxnScript() ;
+
+		
+	    // Traitement des options de la ligne de commandes
+
+            if ( cl.hasOption('c') )
+			{
+				// TO DO
+			}
+
+            if ( cl.hasOption('r') )
+			{
+				// TO DO
+			}
+
+		
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -u -id 178 -nom MER -cp 41500
+            if ( cl.hasOption('u') )
+			{
+					System.out.println( "\n\nMettre a jour encore le code postal dans la base\n" ) ;
+					String sId = cl.getOptionValue("id") ;
+					Integer id = Integer.parseInt(sId);
+					String nom = cl.getOptionValue("nom") ;
+					String sCp = cl.getOptionValue("cp") ;
+					Integer cp = Integer.parseInt(sCp);
+					display = txn.updateVille ( id, nom, cp ) ;
+					System.out.println( display ) ;
+            }
+			
+            if ( cl.hasOption('d') )
+			{
+				// TO DO
+			}
+			
+
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -e
+            if ( cl.hasOption('z') )
+			{
+					boolean the_result ;
+					System.out.println( "\n\nEcraser tables de la base : villes, ... \n" ) ;
+					the_result = TxnScript.eraseDb () ;
+            }
+
+			if ( cl.hasOption('i') )
+			{
+			    txn.initDb() ;
+			}
+
+			if ( cl.hasOption('p') )
+			{
+			    txn.populateDb() ;
+			}
+			
+			
+            if ( cl.hasOption('v') )
+			{
+					System.out.println( "\n\nVerifier acces a la base \n" ) ;
+					
+					boolean bCheck = txn.check () ; 
+					// System.out.println( display ) ;
+            }
+
+
+			
+			// java -cp ".\postgresql-42.2.5.jar;commons-cli-1.4.jar;." simplissimeCmdline -l
+            if ( cl.hasOption('l') )
+			{
+					System.out.println( "\n\nEtat initial de la base\n" ) ;
+					
+					TxnScript.VillesIterator myVilleIt = txn.getIterator4listerVilles() ;
+					int myId ;
+					int myCurrentCodePostal ;
+					String myCurrentNom ;
+
+					display = "" ;		
+					while ( myVilleIt.hasNext() )
+					{
+						myId = myVilleIt.nextIdVille() ;
+						myCurrentCodePostal = myVilleIt.nextCodePostalVille ( ) ;		
+						myCurrentNom = myVilleIt.nextNomVille ( ) ;
+						
+						display = display + "/" + myId ;
+						display = display + "/" + myCurrentCodePostal ;
+						display = display + "/" + myCurrentNom ;
+						display = display + saut_de_ligne ;						
+
+					}
+					System.out.println( display ) ;
+            }
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
+	}
+}
